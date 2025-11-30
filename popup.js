397eed66841a16f6092feb4a439fdb2082d1ac5a
koromoko10 <136338615+koromoko10@users.noreply.github.com>
2025-11-30 18:36:37 +0900

    popupとoptionsのデザインを調整
    ・ポップアップに設定ボタン追加
    ・オプションページのデザインを作成
    （Windows UI）

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
 // 削除されたコメント数を取得して表示
 chrome.storage.local.get("deleteCount", (data) => {
     document.getElementById("deleteCount").textContent = data.deleteCount || 0;
-  });
-  
+});
+
+document.querySelector('#go-to-options').addEventListener('click', function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    windows.open(chrome.runtime.getURL('options.html'));
+  }
+});
